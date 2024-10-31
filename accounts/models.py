ace20926dae709f3f24839c87ae010ce90d35686
javierlnmn <jlm03123@gmail.com>
2024-10-31 12:55:13 +0100

    Custom user model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.templatetags.static import static
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    profile_picture = models.ImageField(upload_to='profile_pictures/', blank=True, null=True)
+
+    @property
+    def profile_picture_url(self):
+        print(static('images/default_user_avatar.png'))
+        return self.profile_picture.url if self.profile_picture else static('images/default_user_avatar.png')
+
+    def __str__(self):
+        return f'{self.username} ({self.first_name} {self.last_name})'
