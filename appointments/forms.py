3e1ff3bc9f06e4d11b82fca26b6416d0e858014a
javierlnmn <jlm03123@gmail.com>
2024-11-02 10:57:57 +0100

    Appointment confirmation page

--- appointments/forms.py
+++ appointments/forms.py
@@ -2,7 +2,7 @@ from django.core.exceptions import ValidationError
 from django import forms
 from django.utils import timezone
 
-from .models import Appointment
+from .models import Appointment, Holiday
 
 class AppointmentForm(forms.ModelForm):
     
@@ -17,6 +17,11 @@ class AppointmentForm(forms.ModelForm):
         appointment_date = cleaned_data.get('appointment_date')
 
         if appointment_date < timezone.now().date():
+            raise ValidationError({'appointment_date': ['This date is not a valid date.']})
+
+        is_holiday = Holiday.objects.filter(date=appointment_date).exists()
+            
+        if is_holiday:
             raise ValidationError({'appointment_date': ['This date is not a valid date.']})
 
         appointment_time = cleaned_data.get('appointment_time')
